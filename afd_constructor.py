5347a18d7c8899c39d574c22af4df599e2c31118
Diego Flores <floresdiego041@gmail.com>
2026-02-08 01:34:38 -0600

    feat: add nullable, firspos, lastpos and followpos

--- afd_constructor.py
+++ afd_constructor.py
@@ -85,8 +85,60 @@ def build_tree(postfix, tree):
 
     return stack.pop()
 
-#Debug para el arbol con la regex seleccionada
-my_tree = RegexTree()
-my_tree.root = build_tree(to_postfix(insert_concat('(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*')), my_tree)
-my_tree.print_tree(my_tree.root)
+def do_nullable_first_last(node):
+    if node is None:
+        return
 
+    do_nullable_first_last(node.left)
+    do_nullable_first_last(node.right)
+
+    if node.node_type == NodeType.SYMBOL:
+        node.nullable = False
+        node.firstpos = {node.position}
+        node.lastpos = {node.position}
+
+    elif node.node_type == NodeType.UNION:
+        node.nullable = node.left.nullable or node.right.nullable
+        node.firstpos = node.left.firstpos | node.right.firstpos
+        node.lastpos = node.left.lastpos | node.right.lastpos
+
+    elif node.node_type == NodeType.CONCAT:
+        node.nullable = node.left.nullable and node.right.nullable
+
+        if node.left.nullable:
+            node.firstpos = node.left.firstpos | node.right.firstpos
+        else:
+            node.firstpos = node.left.firstpos
+
+        if node.right.nullable:
+            node.lastpos = node.left.lastpos | node.right.lastpos
+        else:
+            node.lastpos = node.right.lastpos
+
+    elif node.node_type == NodeType.STAR:
+        node.nullable = True
+        node.firstpos = node.left.firstpos
+        node.lastpos = node.left.lastpos
+
+def do_followpos(node, followpos):
+    if node is None:
+        return
+
+    do_followpos(node.left, followpos)
+    do_followpos(node.right, followpos)
+
+    if node.node_type == NodeType.CONCAT:
+        for i in node.left.lastpos:
+            followpos[i] |= node.right.firstpos
+
+    elif node.node_type == NodeType.STAR:
+        for i in node.left.lastpos:
+            followpos[i] |= node.left.firstpos
+
+#Debug para el arbol con la regex seleccionada
+my_regex = '(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*'
+my_tree = RegexTree()
+my_tree.root = build_tree(to_postfix(insert_concat(expand_regex(my_regex))), my_tree)
+do_nullable_first_last(my_tree.root)
+do_followpos(my_tree.root, my_tree.followpos)
+my_tree.print_tree(my_tree.root)
